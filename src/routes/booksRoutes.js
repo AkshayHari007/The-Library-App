149a3de3d336a12e5b4be635e3687d4167b156e0
AkshayHari007 <akshayhari321@gmail.com>
2022-03-12 16:56:23 +0530

    Single Book

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -2,15 +2,25 @@ const express = require("express");
 const booksRouter = express.Router();
 const Bookdata = require("../model/booksData");
 
-// booksRouter.get("/", function (req, res) {
-//     res.render('books');
-// });
 
 booksRouter.get("/", function (req, res) {
     Bookdata.find().then(function (books) {
-        console.log(books);
+        // console.log(books);
         res.render("books", {
             books
+        });
+    });
+});
+
+// ! for single access on clicking the read more
+booksRouter.get("/:id", function (req, res) {
+
+    const id = req.params.id;
+    Bookdata.findOne({
+        _id: id
+    }).then(function (book) {
+        res.render("book", {
+            book
         });
     });
 });
